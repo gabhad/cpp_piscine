a1a54afbf1de1711444366cbb8b52e17522242dc
gabhad <gabriel@haddad.re>
2020-04-19 14:47:47 +0200

    structure ex01 done. need to code functions

--- day02/ex01/Fixed.class.cpp
+++ day02/ex01/Fixed.class.cpp
@@ -5,6 +5,16 @@ Fixed::Fixed() : _value(0)
     std::cout << "Default constructor called" << std::endl;
 }
 
+Fixed::Fixed(const int n)
+{
+    std::cout << "Int constructor called" << std::endl;
+}
+
+Fixed::Fixed(const float n)
+{
+    std::cout << "Float constructor called" << std::endl;
+}
+
 Fixed::Fixed(Fixed const & src)
 {
     std::cout << "Copy constructor called" << std::endl;
@@ -13,18 +23,27 @@ Fixed::Fixed(Fixed const & src)
 
 Fixed::~Fixed()
 {
-    std::cout << "Default destructor called" << std::endl;
+    std::cout << "Destructor called" << std::endl;
 }
 
 int     Fixed::getRawBits(void) const
 {
-    std::cout << "getRawBits member funciton called" << std::endl;
     return this->_value;
 }
 
 void    Fixed::setRawBits(int const raw)
 {
     this->_value = raw;
+}
+
+float   Fixed::toFloat(void) const
+{
+    return 0;
+}
+
+int     Fixed::toInt(void) const
+{
+    return roundf(this->_value);
 }
     
 Fixed   &Fixed::operator=(Fixed const &rhs)
@@ -34,3 +53,11 @@ Fixed   &Fixed::operator=(Fixed const &rhs)
         this->_value = rhs.getRawBits();
     return *this;
 }
+
+std::ostream    &operator<<(std::ostream &o, Fixed const &rhs)
+{
+    o << rhs.toFloat();
+    return o;
+}
+
+const int   Fixed::_NB_FRAC_BITS = 8;
--- day02/ex01/Fixed.class.hpp
+++ day02/ex01/Fixed.class.hpp
@@ -2,23 +2,31 @@
 # define FIXED_CLASS_HPP
 
 #include <iostream>
+#include <cmath>
 
 class Fixed
 {
 private:
     int                 _value;
 
-    static const int    _NB_FRAC_BITS = 8;
+    static const int    _NB_FRAC_BITS;
     
 public:
     Fixed();
+    Fixed(const int n);
+    Fixed(const float n);
     Fixed(Fixed const & src);
     ~Fixed();
 
-    int     getRawBits(void) const;
-    void    setRawBits(int const raw);
+    int             getRawBits(void) const;
+    void            setRawBits(int const raw);
+    float           toFloat(void) const;
+    int             toInt(void) const;
 
-    Fixed   &operator=(Fixed const &rhs);
+    Fixed           &operator=(Fixed const &rhs);
 };
+
+std::ostream    &operator<<(std::ostream &o, Fixed const &rhs);
+
 
 #endif
