0c58315416bb91eab6f498b2fbe1859c2f934b53
gabhad <gabriel@haddad.re>
2020-04-19 09:55:00 +0200

    ex01 structure done

---
+++ day02/ex01/Fixed.class.cpp
@@ -0,0 +1,36 @@
+#include "Fixed.class.hpp"
+
+Fixed::Fixed() : _value(0)
+{
+    std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(Fixed const & src)
+{
+    std::cout << "Copy constructor called" << std::endl;
+    *this = src;
+}
+
+Fixed::~Fixed()
+{
+    std::cout << "Default destructor called" << std::endl;
+}
+
+int     Fixed::getRawBits(void) const
+{
+    std::cout << "getRawBits member funciton called" << std::endl;
+    return this->_value;
+}
+
+void    Fixed::setRawBits(int const raw)
+{
+    this->_value = raw;
+}
+    
+Fixed   &Fixed::operator=(Fixed const &rhs)
+{
+    std::cout << "Assignation operator called" << std::endl;
+    if (this != &rhs)
+        this->_value = rhs.getRawBits();
+    return *this;
+}
---
+++ day02/ex01/Fixed.class.hpp
@@ -0,0 +1,24 @@
+#ifndef FIXED_CLASS_HPP
+# define FIXED_CLASS_HPP
+
+#include <iostream>
+
+class Fixed
+{
+private:
+    int                 _value;
+
+    static const int    _NB_FRAC_BITS = 8;
+    
+public:
+    Fixed();
+    Fixed(Fixed const & src);
+    ~Fixed();
+
+    int     getRawBits(void) const;
+    void    setRawBits(int const raw);
+
+    Fixed   &operator=(Fixed const &rhs);
+};
+
+#endif
