7aebb21f70534f8ee13c02ee5123da647f964dc6
gabhad <gabriel@haddad.re>
2020-04-20 09:19:55 +0200

    ex02 done

---
+++ day02/ex02/main.cpp
@@ -0,0 +1,30 @@
+#include "Fixed.class.hpp"
+
+int main( void ) { 
+    Fixed a;
+    Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+    std::cout << a << std::endl;
+    std::cout << ++a << std::endl;
+    std::cout << a << std::endl;
+    std::cout << a++ << std::endl;
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+    std::cout << Fixed::max( a, b ) << std::endl;
+
+    // -- my implemented tests
+	std::cout << "--- TESTS of mine ---" << std::endl;
+	std::cout << "Values: a(" << a << ") | b(" << b << ")" << std::endl;
+	std::cout << "Eval: (a > b) == " << (a > b ? "yes" : "no") << std::endl;
+	std::cout << "Eval: (a < b) == " << (a < b ? "yes" : "no") << std::endl;
+	std::cout << "Eval: (a >= b) == " << (a >= b ? "yes" : "no") << std::endl;
+	std::cout << "Eval: (a <= b) == " << (a <= b ? "yes" : "no") << std::endl;
+	std::cout << "Eval: (a != b) == " << (a != b ? "yes" : "no") << std::endl;
+	std::cout << "Eval: (a == b) == " << (a == b ? "yes" : "no") << std::endl;
+	std::cout << "Arithmetics time!" << std::endl;
+	std::cout << "Eval: (a - b) = " << Fixed(a - b) << std::endl;
+	std::cout << "Eval: (a + b) = " << Fixed(a + b) << std::endl;
+	std::cout << "Eval: (a * b) = " << Fixed(a * b) << std::endl;
+	std::cout << "Eval: (a / b) = " << Fixed(a / b) << std::endl;
+
+    return 0;
+}
