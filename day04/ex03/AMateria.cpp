f874201308788d62061b80faa4499bb3f4581ac4
gabhad <gabriel@haddad.re>
2020-04-27 16:00:20 +0200

    files created ex03

---
+++ day04/ex03/AMateria.cpp
@@ -0,0 +1,21 @@
+#include "AMateria.hpp"
+
+AMateria::AMateria(/*args*/)
+{
+}
+
+AMateria::AMateria()
+{
+}
+
+AMateria::AMateria(const AMateria &)
+{
+}
+
+AMateria::~AMateria()
+{
+}
+
+AMateria &AMateria::operator=(const AMateria &)
+{
+}
---
+++ day04/ex03/AMateria.hpp
@@ -0,0 +1,16 @@
+#ifndef AMATERIA_HPP
+# define AMATERIA_HPP
+
+class AMateria
+{
+    private:
+        /* args */
+    public:
+        AMateria(/* args */);
+        AMateria();
+        AMateria(const AMateria &);
+        ~AMateria();
+        AMateria &operator=(const AMateria &);
+};
+
+#endif
