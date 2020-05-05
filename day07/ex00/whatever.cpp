51007fea3d85df1b012ac00ad088921d8f3df5a5
gabhad <gabriel@haddad.re>
2020-05-05 16:23:08 +0200

    changed main

--- day07/ex00/whatever.cpp
+++ day07/ex00/whatever.cpp
@@ -17,16 +17,19 @@ T const &min(T const & x, T const & y) { return ((x<=y) ? x : y);}
 
 //*******************************************************//
 
-int main(void)
+int main()
 {
-    int x = 5;
-    int y = 0;
-    std::cout << "x = " << x << std::endl;
-    std::cout << "y = " << y << std::endl;
-    swap(x, y);
-    std::cout << "x = " << x << std::endl;
-    std::cout << "y = " << y << std::endl;
-    std::cout << "max(x,y) = " << max(x,y) << std::endl;
-    std::cout << "min(x,y) = " << min(x,y) << std::endl;
+    int a = 2;
+    int b = 3;
+    ::swap( a, b );
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
     return 0;
 }
