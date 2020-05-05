5cb80d75e6406f4df6c8c4659249d3ebeafa3d44
gabhad <gabriel@haddad.re>
2020-05-05 13:39:35 +0200

    ex01 done

--- day06/ex01/main.cpp
+++ day06/ex01/main.cpp
@@ -1,16 +1,54 @@
-#include "Data.hpp"
+#include <iostream>
 
-Data    *deserialize(void *raw)
+struct  Data
 {
+    std::string s1;
+    int         n;
+    std::string s2;
+};
 
+std::string randomString(void)
+{
+    std::string str("");
+    std::string abc("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
+
+    for (int i = 0; i < 8; i++)
+        str += abc[static_cast<unsigned long>(rand()) %62];
+    str[8] = 0;
+    return str;
 }
 
-void    *serialize(void)
+Data        *deserialize(void *raw)
 {
+    Data *data = reinterpret_cast<Data *>(raw);
 
+    std::cout << "deserialization: " << std::endl;
+    std::cout << "n = " << data->n << std::endl;
+    std::cout << "s1 = " << data->s1 << std::endl;
+    std::cout << "s2 = " << data->s2 << std::endl;
+    return data;
 }
 
-int main()
+void        *serialize(void)
 {
-    
+    Data    *data = new Data;
+
+    srand(static_cast<unsigned int>(time(NULL)));
+    data->n = rand();
+    data->s1 = randomString();
+    data->s2 = randomString();
+    std::cout << "n = " << data->n << std::endl;
+    std::cout << "s1 = " << data->s1 << std::endl;
+    std::cout << "s2 = " << data->s2 << std::endl;
+    return reinterpret_cast<void *>(data);
+}
+
+int         main()
+{
+    Data    *data;
+    void    *ptr;
+
+    ptr = serialize();
+    data = deserialize(ptr);
+    delete data;
 }
