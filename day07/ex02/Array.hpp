1f6fa65c39f711d1ca6370bd2db6566957c2354a
gabhad <gabriel@haddad.re>
2020-05-06 09:59:38 +0200

    last corrections to make

--- day07/ex02/Array.hpp
+++ day07/ex02/Array.hpp
@@ -7,37 +7,51 @@
 template <typename T>
 class Array
 {
-    private:
-        T   _array[];
-        int _size;
     public:
-        Array(unsigned int n) : _size(n) { this->_array = new T[_size]; };
+        Array(unsigned int n) : _size(n) 
+        { 
+            for (unsigned int i = 0; i < n; i++)
+            {
+                this->_array[i] = *new T;
+                this->_array[i] = 0;
+            } 
+        };
         Array() : _size(0) {};
         Array(const Array &src) { *this = src; };
-        ~Array() { delete[] this->array; };
-        Array &operator=(const Array &rhs) {
-            if (this != &rhs) {
+        ~Array() {};
+        Array &operator=(const Array &rhs) 
+        {
+            if (this != &rhs) 
+            {
+                this->_array = new T[rhs.size];
                 for (int i = 0; i < this->_size; i++)
                     this[i] = rhs[i];
                 this->_size = rhs._size;
-            return *this; }
+            return *this;
+            }
         };
 
         int size() const { return this->_size; };
 
-        T &operator[](int idx) { 
+        T   &operator[](unsigned int idx) 
+        { 
             try
             {
                 if (idx >= this->_size)
+                {
                     throw ( std::exception() );
-                else
-                    return this->_array[idx];
+                }
             }
             catch(const std::exception& e)
             {
                 std::cerr << "Error : out of bounds" << std::endl;
+                exit (0);
             }
+            return this->_array[idx];
         }
+    private:
+        unsigned int    _size;
+        T               _array[];
 };
 
 #endif
--- day07/ex02/main.cpp
+++ day07/ex02/main.cpp
@@ -1,8 +1,18 @@
 #include "Array.hpp"
 #include <iostream>
 
-int main()
+int main (void)
 {
-    Array *array = new Array;
-    (void)array;
+	Array<int>		arr(1);
+    std::cout << "arr[0] = " << arr[0] << std::endl;
+    arr[0] = 1;
+    std::cout << "arr[0] = " << arr[0] << std::endl;
+    std::cout << "Now I will try to add 1th element, should throw exception !" << std::endl;
+    arr[1] = 2;
+    std::cout << arr[1] << std::endl;
+	std::cout << "*** Will to do something with an empty array ***" << std::endl;
+    std::cout << "*Creating an empty array" << std::endl;
+    Array<int>		test;
+    std::cout << "*Trying to reach non existing item" << std::endl;
+    std::cout << test[10] << std::endl;
 }
