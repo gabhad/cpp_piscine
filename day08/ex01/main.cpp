79c1c045c32af0851fb051cfc9f43efb235bb2af
gabhad <gabriel@haddad.re>
2020-05-07 08:53:44 +0200

    ex01 done

--- day08/ex01/main.cpp
+++ day08/ex01/main.cpp
@@ -3,12 +3,35 @@
 
 int main() 
 {
-    Span sp = Span(5);
-    sp.addNumber(5);
-    sp.addNumber(3);
-    sp.addNumber(17);
-    sp.addNumber(9);
-    sp.addNumber(11);
-    std::cout << sp.shortestSpan() << std::endl;
-    std::cout << sp.longestSpan() << std::endl;
+    Span sp = Span(1);
+    try
+    {
+        sp.addNumber(1);
+        sp.addNumber(2);
+
+    }
+    catch(const Span::StockageOverloadException& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+    
+    Span sp2 = Span(10000);
+    for (int i = 0; i < 10000; i++)
+        sp2.addNumber(rand());
+    try
+    {    
+        std::cout << sp2.shortestSpan() << std::endl;
+    }
+    catch(const Span::NoSpanToBeFoundException& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+    try
+    {
+        std::cout << sp2.longestSpan() << std::endl;
+    }
+    catch(const Span::NoSpanToBeFoundException& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
 }
--- day08/ex01/span.cpp
+++ day08/ex01/span.cpp
@@ -1,4 +1,6 @@
 #include "span.hpp"
+#include <iostream>
+#include <vector>
 
 Span::Span(unsigned int num) : _numberOfInts(num)
 {
@@ -18,7 +20,7 @@ Span &Span::operator=(const Span & rhs)
     if (this != &rhs)
     {
         this->_numberOfInts = rhs._numberOfInts;
-        for (int i = 0; i < rhs._elements.size; i++)
+        for (unsigned int i = 0; i < rhs._elements.size(); i++)
             this->addNumber(rhs._elements[i]);
     }
     return *this;
@@ -26,31 +28,42 @@ Span &Span::operator=(const Span & rhs)
 
 void    Span::addNumber(int n)
 {
-    if (this->_elements.size == this->_numberOfInts)
+    if (this->_elements.size() == this->_numberOfInts)
         throw Span::StockageOverloadException();
     this->_elements.push_back(n);
 }
 
 int     Span::shortestSpan()
 {
-    if (_elements.size < 2)
+    if (_elements.size() < 2)
         throw Span::NoSpanToBeFoundException();
-    // use sort to rearrange
-    // calculate the difference between each element, and store the minimum.
-    // return final minimum
+    int span;
+    std::sort(this->_elements.begin(), this->_elements.end());
+    std::vector<int>::iterator it1 = this->_elements.begin();
+    std::vector<int>::iterator it2 = this->_elements.begin() + 1;
+    span = *it2 - *it1;
+    while (it2 != this->_elements.end())
+    {
+        if (*it2 - *it1 < span)
+            span = *it2 - *it1;
+        it1++;
+        it2++;
+    }
+    return span;
 }
 
 int     Span::longestSpan()
 {
-    if (_elements.size < 2)
+    if (_elements.size() < 2)
         throw Span::NoSpanToBeFoundException();
-    // use min and max 
-    // 
+    std::vector<int>::iterator max = std::max_element(this->_elements.begin(), this->_elements.end());
+    std::vector<int>::iterator min = std::min_element(this->_elements.begin(), this->_elements.end());
+    return (*max - *min);
 }
 
 const char* Span::StockageOverloadException::what() const throw()
 {
-    return ("Error : You cannot add any more element to your class.");
+    return ("Error : You cannot add any more element.");
 }
 
 const char* Span::NoSpanToBeFoundException::what() const throw()
